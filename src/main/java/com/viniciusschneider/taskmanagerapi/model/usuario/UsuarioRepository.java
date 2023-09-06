87f526d73fa45151a53c5525af6d3cc6235fabe7
Vinícius Mombach Schneider <vinicius.schneider@cwi.com.br>
2023-09-05 23:31:30 -0300

    Adicionando spring security

--- src/main/java/com/viniciusschneider/taskmanagerapi/model/usuario/UsuarioRepository.java
+++ src/main/java/com/viniciusschneider/taskmanagerapi/model/usuario/UsuarioRepository.java
@@ -3,9 +3,12 @@ package com.viniciusschneider.taskmanagerapi.model.usuario;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
     Page<Usuario> findAllByAtivoTrue(Pageable pageable);
+
+    UserDetails findByLogin(String username);
 }
---
+++ src/main/java/com/viniciusschneider/taskmanagerapi/model/usuario/service/AutenticacaoService.java
@@ -0,0 +1,19 @@
+package com.viniciusschneider.taskmanagerapi.model.usuario.service;
+
+import com.viniciusschneider.taskmanagerapi.model.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
